dc59254c94f072d4908d560ecb4df400eaa9f86c
Antbert1 <anthea4002@gmail.com>
2022-04-28 18:59:35 +0100

    Set up redux

---
+++ src/clothingData.js
@@ -0,0 +1,93 @@
+var clothingData = [
+    {
+        name: "Fleece, Red",
+        category: "Womens hiking",
+        price: "32",
+        quantity: 4,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Boots, Brown",
+        category: "Womens hiking",
+        price: "99",
+        quantity: 10,
+        image: "https://cdn.pixabay.com/photo/2016/09/02/11/10/boots-1638873_960_720.jpg"
+    },
+    {
+        name: "Fleece, Green",
+        category: "Mens hiking",
+        price: "34",
+        quantity: 6,
+        image: "https://cdn.pixabay.com/photo/2017/05/26/14/58/fabric-2346211_960_720.jpg"
+    },
+    {
+        name: "Boots, Black",
+        category: "Mens hiking",
+        price: "99",
+        quantity: 9,
+        image: "https://cdn.pixabay.com/photo/2018/11/30/01/03/winter-boots-3846863_960_720.jpg"
+    },
+    {
+        name: "Vest, Yellow",
+        category: "Womens running",
+        price: "26",
+        quantity: 4,
+        image: "https://cdn.pixabay.com/photo/2018/02/02/04/48/sweater-3124635_960_720.jpg"
+    },
+    {
+        name: "Jacket, Black",
+        category: "Womens running",
+        price: "75",
+        quantity: 2,
+        image: "https://cdn.pixabay.com/photo/2016/11/29/03/15/man-1867009_960_720.jpg"
+    },
+    {
+        name: "T-shirt, Yellow",
+        category: "Mens running",
+        price: "19.99",
+        quantity: 4,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Jacket, Grey",
+        category: "Mens running",
+        price: "86.00",
+        quantity: 0,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Jacket, Orange",
+        category: "Womens cycling",
+        price: "24.50",
+        quantity: 4,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Bib-tights, Grey",
+        category: "Womens cycling",
+        price: "40.00",
+        quantity: 9,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Bib-shorts, Black",
+        category: "Womens cycling",
+        price: "30.00",
+        quantity: 2,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Jacket, Green",
+        category: "Mens cycling",
+        price: "55.00",
+        quantity: 1,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+    {
+        name: "Bib-tights, Black",
+        category: "Mens cycling",
+        price: "65.00",
+        quantity: 5,
+        image: "https://cdn.pixabay.com/photo/2016/10/07/19/05/girl-1722402_960_720.jpg"
+    },
+]
